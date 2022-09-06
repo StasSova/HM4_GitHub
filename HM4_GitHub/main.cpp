5c9a183a4cd1d2a71d67ad410f53d04f2203e109
Стас Сова <stassova2017@gmail.com>
2022-09-06 15:33:36 +0300

    Создан конструктор с параметрами.

--- HM4_GitHub/main.cpp
+++ HM4_GitHub/main.cpp
@@ -13,6 +13,14 @@ public:
 		age = 0;
 		number = 0;
 	}
+	Student(const char* NewName, int Age, int Number)
+	{
+		name = new char[strlen(NewName) + 1];
+		strcpy_s(name, strlen(NewName) + 1, NewName);
+
+		age = Age;
+		number = Number;
+	}
 };
 
 
